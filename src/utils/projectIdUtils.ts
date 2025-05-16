f6e2517a17a05f6b6cc06f304de2c92c67b9251d
HamdBilalTahir <ht818@nyu.edu>
2025-05-16 07:42:01 +0400

    Fixed rules

--- src/utils/projectIdUtils.ts
+++ src/utils/projectIdUtils.ts
@@ -1,8 +1,8 @@
 export function generateAndSetProjectId() {
-  const storedProjectId = localStorage.getItem("projectId");
+  const storedProjectId = localStorage.getItem('projectId');
   if (!storedProjectId) {
-    const newProjectId = "_" + Math.random().toString(36).substr(2, 9);
-    localStorage.setItem("projectId", newProjectId);
+    const newProjectId = '_' + Math.random().toString(36).substr(2, 9);
+    localStorage.setItem('projectId', newProjectId);
     return newProjectId;
   }
   return storedProjectId;
